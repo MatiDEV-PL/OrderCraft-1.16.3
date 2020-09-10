ceda7abb67399637e9be38f976b06feaa4512220
MatiDEV-PL <mateuszmalczewski5@gmail.com>
2020-09-10 21:41:26 +0200

    Forege updated to 1.16.2-33.0.61

---
+++ src/main/java/com/example/adam/world/gen/ModOreGen.java
@@ -0,0 +1,18 @@
+package com.example.adam.world.gen;
+
+import com.example.adam.ToolMod;
+import net.minecraft.block.BlockState;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = ToolMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModOreGen { }
